19ca29687b9879f00d7e6facc07253cf8d48a3b0
William Estrada <will@futuriza.me>
2024-05-31 00:26:12 -0500

    adding loading when sending form early access

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { MainContainer } from "./ui/main-container";
 import { RedirectEarlyAccessForm } from "./ui/redirect-early-access-form";
 import { Logo } from "./ui/logo";
+import { Presentation } from "./ui/presentation";
+import { About } from "./ui/about";
 
 export default function App() {
   return (
     <MainContainer>
       <Logo />
+      <Presentation />
+      <About />
       <RedirectEarlyAccessForm />
     </MainContainer>
   );
