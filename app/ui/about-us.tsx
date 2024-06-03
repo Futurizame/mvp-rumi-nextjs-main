b79e68382666b7b70bbc88c0f7e2f9e8526a71e7
William Estrada <will@futuriza.me>
2024-06-02 22:13:53 -0500

    updated colors and style for the landing

--- app/ui/about-us.tsx
+++ app/ui/about-us.tsx
@@ -1,12 +1,12 @@
 import { SectionContainer } from "./section-container";
 import React, { FC } from "react";
-import Link from "next/link";
+import { RedirectButton } from "./redirect-button";
 
 export const AboutUs: FC = () => {
   return (
-    <SectionContainer containerClassName="p-4">
-      <h2>SOBRE NOSOTROS</h2>
-      <p className="text-gray-500">
+    <SectionContainer containerClassName="p-4" className={"flex flex-col"}>
+      <h2 className={"text-primary text-xl font-bold pt-4"}>SOBRE NOSOTROS</h2>
+      <p className="text-text font-bold text-xl pt-2">
         Creemos en el poder de las inversiones inmobiliarias para construir un
         futuro mejor. Somos una plataforma que conecta a inversores con
         oportunidades inmobiliarias cuidadosamente seleccionadas, permitiendo
@@ -14,29 +14,31 @@ export const AboutUs: FC = () => {
         participen en un mercado que antes era exclusivo para un público
         especializado y con el capital suficiente.
       </p>
-      <h2>NUESTRA MISIÓN</h2>
-      <p className="text-gray-500">
+
+      <h2 className={"text-primary text-xl font-bold pt-8"}>NUESTRA MISIÓN</h2>
+      <p className="text-text font-bold text-xl pt-2">
         Democratizar el acceso a las inversiones inmobiliarias y hacer que sea
         más fácil que nunca construir su patrimonio y alcanzar sus objetivos
         financieros.
       </p>
-      <h2>¿QUÉ OFRECEMOS?</h2>
-      <p className="text-gray-500">
-        Oportunidades de inversión diversificadas Proceso de inversión sencillo
-        Tasas justas (más bajas del mercado) Equipo experimentado en el mercado
-        inmobiliario Estamos apasionados por ayudar a las personas a alcanzar
-        sus sueños financieros y sabemos que las inversiones inmobiliarias son
-        una excelente manera de hacerlo porque son inversiones estables y de
-        bajo riesgo. Con Rumigrow, puedes invertir en ellas de manera accesible
-        y construir un legado duradero para tu futuro.
+
+      <h2 className={"text-primary text-xl font-bold pt-8"}>¿QUÉ OFRECEMOS?</h2>
+
+      <p className="text-text font-bold text-xl pt-2">
+        <ul className={"list-disc pl-4 pb-4"}>
+          <li>Oportunidades de inversión diversificadas</li>
+          <li>Proceso de inversión sencillo</li>
+          <li>Tasas justas (más bajas del mercado)</li>
+          <li>Equipo experimentado en el mercado inmobiliario</li>
+        </ul>
+        Estamos apasionados por ayudar a las personas a alcanzar sus sueños
+        financieros y sabemos que las inversiones inmobiliarias son una
+        excelente manera de hacerlo porque son inversiones estables y de bajo
+        riesgo. Con Rumigrow, puedes invertir en ellas de manera accesible y
+        construir un legado duradero para tu futuro.
       </p>
 
-      <Link
-        href={"early-access"}
-        className={"block w-max bg-sky-700 text-white py-4 px-8 rounded-full"}
-      >
-        ¡Quiero invertir!
-      </Link>
+      <RedirectButton className={"pt-8 self-center pb-16"} />
     </SectionContainer>
   );
 };
