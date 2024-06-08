ac5bec3ae2de3b32d94ff0bfeebb26a4373ae00f
Will <will@futuriza.me>
2024-06-07 19:02:19 -0500

    added footer (#18)

--- app/early-access/page.tsx
+++ app/early-access/page.tsx
@@ -1,11 +1,13 @@
 import { Logo } from "../ui/logo";
 import { FormContainer } from "./ui/form-container/form-container";
+import { Footer } from "../ui/footer";
 
 export default function EarlyAccess() {
   return (
     <main>
       <Logo redirect={"/"} />
-      <FormContainer />;
+      <FormContainer />
+      <Footer />
     </main>
   );
 }
---
+++ app/ui/footer.tsx
@@ -0,0 +1,24 @@
+import image from "./logo-white.png";
+import Image from "next/image";
+import { clsx } from "clsx";
+import { SectionContainer } from "./section-container";
+
+export const Footer = () => {
+  return (
+    <SectionContainer
+      containerClassName={"bg-text text-white"}
+      className={clsx(
+        "flex",
+        "flex-col gap-8 p-4 py-12",
+        "sm:flex-row sm:justify-between sm:items-center sm:p-12",
+      )}
+    >
+      <Image src={image} alt={"logo rumigrow"} width={250} />
+      <div className={"flex flex-col gap-3"}>
+        <h6 className={"font-bold"}>Contacto</h6>
+        <h6>contacto@rumigrow.com</h6>
+        <h6>999 999 999</h6>
+      </div>
+    </SectionContainer>
+  );
+};
