5537fd18d504a2903ee640671c553281aea366de
adhemir sabino hilario <58167833+Adhemirsab@users.noreply.github.com>
2024-06-13 19:51:03 -0500

    feat: :sparkles: added google analytics (#26)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="default">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_MEASUREMENT_ID || ""} />
     </html>
   );
 }
